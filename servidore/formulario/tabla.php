f773c0805a966a20089fba11a388f228b1763103
Jose Maria Gallego <josema.m.g.95@hotmail.com>
2018-10-16 19:30:18 +0200

    Clase Martes 16/10/2018

---
+++ servidore/formulario/tabla.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tabla de multiplicar</title>
+  </head>
+  <body>
+    <?php $numero = 3; ?>
+
+    <table border='1'>
+      <thead>
+          <th><?= $numero ?></th>
+          <th>x</th>
+          <th>n</th>
+          <th>=</th>
+          <th>m</th>
+    </thead>
+      <tbody>
+        <?php for ($i = 0; $i < 10; $i++): ?>
+            <tr>
+              <td><?= $numero ?></td>
+              <td>x</td>
+              <td><?= $i ?></td>
+              <td>=</td>
+              <td><?= $numero * $i ?></td>
+            </tr>
+        <?php endfor ?>
+      </tbody>
+    </table>
+
+  </body>
+</html>
