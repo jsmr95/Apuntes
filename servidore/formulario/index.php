1141ad3518fbb85ebcb7e4a7de87920a8a9b0d69
Jose Maria Gallego <josema.m.g.95@hotmail.com>
2018-10-19 20:10:56 +0200

    Push 19/10/2018

---
+++ servidore/formulario/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tabla de multiplicar</title>
+  </head>
+  <body>
+      <form action="" method="get">
+          <label for="num">Número:</label>
+          <input id="num" type="text" name="num">
+          <br /> <br />
+          <input type="submit" value="Calcular">
+          <br /><br /> <br />
+      </form>
+    <?php
+        require './auxiliar.php';
+
+        if (isset($_GET['num'])){
+            $numero = $_GET['num'];
+            if (!empty($numero)) {
+                if (!ctype_digit($numero)){
+                    mostrarError('Se ha pasado algo que no es un número');
+                } else {
+                    if ($numero < 0 || $numero > 10){
+                        mostrarError('El numero debe estar entre 0 y 10.');
+                    } else {
+                        mostrarTabla($numero);
+                    }
+                }
+            }
+        }
+    ?>
+  </body>
+</html>
